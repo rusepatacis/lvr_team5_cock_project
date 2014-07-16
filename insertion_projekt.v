aae61f44ace493a2380f0cb632d4fc06669968d9
Jaka Demsar <jaka.demsar0@gmail.com>
2014-07-16 19:53:34 +0200

    Zdruzil dokaza v enega (je razumljivejse).

--- insertion_projekt.v
+++ insertion_projekt.v
@@ -85,31 +85,27 @@ Proof.
               firstorder.
 Qed.
 
-(* ::: Dokaz pravilnosti ::: *)
 
-(* Rezulat insterition sorta je urejen seznam... *)
-Theorem insertion_sort_deluje_1: forall lst: list Z, urejen (insertion_sort lst).
+
+(* ::: Dokaz pravilnosti ::: *)
+(* Rezulat insterition sorta je urejen seznam, ki je obenem permutacija vhodnega seznama *)
+Theorem insertion_sort_deluje: forall lst: list Z, urejen (insertion_sort lst) /\ permutiran lst (insertion_sort lst).
 Proof.
    intro.
-   (*unfold urejen.*)
-   induction lst.
-   - reflexivity. (* Trivialen primer *)
-   - apply ohranjanje_urejenosti_vstavljanja. (* Tu uporabi pomozno lemo *)
-     auto.
-Qed.
-
-(* ... ki je obenem permutacija vhodnega seznama *)
-Theorem insertion_sort_deluje_2: forall lst: list Z, permutiran lst (insertion_sort lst).
-Proof.
-   induction lst.
-   - unfold insertion_sort.
-     apply permutiran_refl. (* to je v BauerSortu *)
-   - destruct lst.
-     + apply permutiran_refl. (* to je v BauerSortu *)
-     + case_eq (a =? z). (* tu uporabi pomozno lemo *)
-       * intro.
-         simpl.
-         admit.
-       * admit.
+   split.
+   - induction lst. (* Prvi del izreka ... UREJENOST *)
+      * reflexivity. (* Trivialen primer *)
+      * apply ohranjanje_urejenosti_vstavljanja. (* Tu uporabi pomozno lemo *)
+        auto.
+   - induction lst. (* Drugi del izreka ... JE PERMUTACIJA *)
+     * unfold insertion_sort.
+       apply permutiran_refl. (* to je v BauerSortu *)
+     * destruct lst.
+        + apply permutiran_refl. (* to je v BauerSortu *)
+        + case_eq (a =? z). (* tu uporabi pomozno lemo *)
+            intro.
+            simpl.
+            admit.
+            admit.
 Qed.
   
