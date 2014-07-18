d27c94155f91ef1c94961f142a02c0571d8007dd
Jani <janibizjak@gmail.com>
2014-07-18 08:17:24 +0200

    Dodan dokaz za permutacijo ter končen dokaz
    Dodan dokaz, ki pove da je permutiran seznam enak originalu.
    Dodan končen dokaz, ki združi dokaz o urejenosti ter permutaciji in
    dokaže insertion sort.

--- insertion_projekt.v
+++ insertion_projekt.v
@@ -62,51 +62,134 @@ Proof.
 
             assumption. (* Vedno predpostavimo resnico *)
        + simpl. (* Seznam sestavljen iz glave in repa *)
-         case (Z_le_gt_dec x z). (* Locimo primere glede na glavo *)
-         simpl.
-           intros.
-           split.
-             firstorder.
-             split.
-                assumption.
-                firstorder.
-         split.
-         firstorder.
-         replace (z :: insert x l) with (insert x (z :: l)). (* Ker zelimo uporabiti hipotezo IHl (njen desni del *)
-         apply urejen_tail in H. (* Poenostavimo H, da ustreza levemu delu IHl in s tem pridobimo 'desni del' IHl *)
-         firstorder.  
-         simpl.
-         firstorder.
-         destruct (Z_le_gt_dec x z). (* V hipotezah imamo ustrezen pogoj, moti nas notacija Z_le_gt_dec *)
-         firstorder.
-         firstorder.
+         case (Z_le_gt_dec x z). (* Locimo primere glede na primerjavo z glavo *)
+           simpl. (* x <= z ; razpise definicijo urejen*)
+           intros. (* Dokazujemo implikacijo *)
+           split. (* Dokaz konjunkcije *)
+             firstorder. (* Telovadba z neenakostmi *)
+             
+             split. (* Dokaz konjunkcije *)
+              assumption.
+              firstorder.
+           
+           split. (* x > z *)
+              firstorder. (* Telovadba z neenakostmi *)
+           
+              replace (z :: insert x l) with (insert x (z :: l)). (* Ker zelimo uporabiti hipotezo IHl (njen desni del *)
+              apply urejen_tail in H. (* Poenostavimo H, da ustreza levemu delu IHl in s tem pridobimo 'desni del' IHl *)
+              firstorder.  (* Ocitno iz implikacije v hipotezah *)
+              simpl. (* Razpis insert *)
+              firstorder. (* Poenostavimo kar se da v hipotezah *)
+              destruct (Z_le_gt_dec x z). (* V hipotezah imamo ustrezen pogoj, moti nas notacija Z_le_gt_dec, zelimo > <= *)
+              firstorder.
+              firstorder.
 Qed.
 
-(* ::: Dokaz pravilnosti ::: *)
 
-(* Rezulat insterition sorta je urejen seznam... *)
-Theorem insertion_sort_deluje_1: forall lst: list Z, urejen (insertion_sort lst).
+
+(* pomozna lema za dokazovanje, da je sortiran seznam enak neurejenemu seznamu,
+ tukaj gledamo primer ko x != y * *)
+Lemma ni_enak (x y : Z) (l : list Z): (x =? y = false) -> pojavi x l = pojavi x (insert y l).
 Proof.
    intro.
-   (*unfold urejen.*)
-   induction lst.
-   - reflexivity. (* Trivialen primer *)
-   - apply ohranjanje_urejenosti_vstavljanja. (* Tu uporabi pomozno lemo *)
-     auto.
+   induction l. (* indukcija po seznamu *)
+   -simpl. (* privzeli smo da x != y *)
+    rewrite H. (* leva stran je enaka desni *)
+    auto.
+   -simpl. (* lociti moramo po x=a in y < a *)
+    case (Z_le_gt_dec y a).
+    +intro. (* y <= a *)
+     case_eq (x =? a). (* x = a *)
+     *intro. (* x = a *)
+      simpl. (* velja x = a in x != y *)
+      rewrite H.
+      rewrite H0. (* leva stran = desni strani *)
+      auto.
+     *intro. (* x != a *)
+      simpl. (* velja x != y in x != a *)
+      rewrite H.
+      rewrite H0. (* leva stran enaka desni *)
+      auto.
+    +intro. (* y > a *)
+     case_eq (x =? a).
+     *intro. (* x = a *)
+      simpl. (* velja x =a *)
+      rewrite H0. (* notranja leva stran je ravno leva stran IHl *)
+      rewrite IHl. (* leva stran je enaka desni *)
+      auto.
+     *intro. (* x != a *)
+      simpl. (* velja x != a *)
+      rewrite H0. (* ocitno je enako *)
+      auto.
 Qed.
 
-(* ... ki je obenem permutacija vhodnega seznama *)
-Theorem insertion_sort_deluje_2: forall lst: list Z, permutiran lst (insertion_sort lst).
+
+(* pomozna lema, gledamo primer ko je x = y *)
+Lemma enak(x y : Z) (l : list Z): (x =? y = true ) -> pojavi x (insert x l) = S (pojavi x l).
 Proof.
-   induction lst.
-   - unfold insertion_sort.
-     apply permutiran_refl. (* to je v BauerSortu *)
-   - destruct lst.
-     + apply permutiran_refl. (* to je v BauerSortu *)
-     + case_eq (a =? z). (* tu uporabi pomozno lemo *)
-       * intro.
-         simpl.
-         admit.
-       * admit.
+   intro.
+   induction l. (*indukcija po seznamu *)
+   -simpl. (* if x = x -> vedno true *)
+    rewrite Z.eqb_refl.
+    auto. (* ocitno *)
+   -simpl. (* locimo glede na x > a in x = a *)
+    case (Z_le_gt_dec x a). (* x <= a *)
+    +intro. (* <= *)
+     case_eq (x =? a). (* x = a *)
+     *intro.
+      simpl. (* velja x = a in ocitno x=x *)
+      rewrite H0.
+      rewrite Z.eqb_refl. (* ocitno je leva stran enaka desni *)
+      auto.
+     *intro. (* x != a *)
+      simpl. (* velja x != a in x = x *)
+      rewrite H0.
+      rewrite Z.eqb_refl. (* ocitno je leva stran enaka desni *)
+      auto.
+    +intro. (* x > a *)
+     case_eq (x =? a).
+     *intro. (* x = a *)
+      simpl. (* velja x = a *)
+      rewrite H0. (* notranja leva stran je ravno IHl leva stran IHl *)
+      rewrite IHl. (*leva stran = desni strani *)
+      auto.
+     *intro. (* x != a *)
+      simpl. (* velja x != a *)
+      rewrite H0. (* ponovno je leva stran enaka IHl *)
+      rewrite IHl.
+      auto. 
+Qed. 
+
+
+(* ::: Dokaz pravilnosti ::: *)
+(* Rezulat insterition sorta je urejen seznam, ki je obenem permutacija vhodnega seznama *)
+Theorem insertion_sort_deluje: forall lst: list Z, urejen (insertion_sort lst)  /\ permutiran lst (insertion_sort lst).
+Proof.
+   intro.
+   split. (* Dokazemo oba dela izreka *)
+   - induction lst. (* Prvi del izreka ... UREJENOST *)
+      * reflexivity. (* Trivialen primer *)
+      * apply ohranjanje_urejenosti_vstavljanja. (* Tu uporabi pomozno lemo *)
+        auto.
+   -  intro. (* Drugi del izreka ... JE PERMUTACIJA *)
+      induction lst. (* indukcija po seznamu *)
+      * reflexivity. (* trivialen primer *)
+      * simpl.      (* potrebno lociti glede na x = a *)
+        case_eq (x =? a).
+           + intro.     (* x = a *)
+             rewrite Z.eqb_sym in H. (* obrnemo ter uporabimo hipotezo H, *)
+             rewrite Z.eqb_eq in H.  (* da se znebimo a iz cilja *)
+             rewrite H.
+	     rewrite (enak x a). (* uporabimo pomozno lemo *)
+	     auto.
+             rewrite H.
+	     rewrite Z.eqb_refl.
+	     auto.
+	   + intro.   (* x != a *)
+             rewrite IHlst. (* razpisemo cilj *)
+	     rewrite (ni_enak x a). (* uporabimo pomozno lemo *)
+	     auto. (* ocitno *)
+	     auto. (* H *)
 Qed.
   
+
